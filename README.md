8977ed6920f5a8f990988eae518cfa0a592c5144
renjiezhou <rzhou10@gmail.com>
2022-10-06 09:23:18 -0400

    Commit:     - Initial commit!

--- README.md
+++ README.md
@@ -1,46 +1,42 @@
-# Getting Started with Create React App
+# Shoppable Pagination
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+This project is the for the internal component for infinite scrolling for Shoppable. This will be used
+for 
 
-## Available Scripts
+## Install
 
-In the project directory, you can run:
+```  
+  npm install --save shoppable-pagination
 
-### `npm start`
+  or
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+  yarn add shoppable-pagination
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+  // in code ES6
+  import InfiniteScroll from 'shoppable-pagination';
+  // or commonjs
+  var InfiniteScroll = require('shoppable-pagination');
+```
 
-### `npm test`
+## props
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+| Name             | Type           | Description                                                   |
+|------------------|----------------|---------------------------------------------------------------|
+| fetch            | Function       | Fetch function to fetch more items                            |
+| hasMore          | boolean        | Whether or not there are more items to fetch                  |
+| searchInProgress | boolean        | Whether or not fetch function is currently fetching new items |
+| items            | Array<T>       | Array of items to map over                                    |
+| renderMap        | Function       | Function to map over items as defined in items parameter      |
+| classNames       | string         | Class names for different parent div for items                |
+| buttonSettings   | ButtonSettings | containing various settings for back to top button            |
+| loaderHTML       | Function       | Function to return HTML for loading                           |
 
-### `npm run build`
+### ButtonSettings
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+| Name   | Type    | Description                                                            |
+|--------|---------|------------------------------------------------------------------------|
+| show   | boolean | Show back to top button                                                |
+| right  | string  | How far to position from the right side of the screen                  |
+| bottom | string  | How far to position from the bottom of the screen                      |
+| limit  | number  | The limit to reveal the back to top button                             |
+| id     | string  | If there is a secondary element that needs to be scrolled for the page |
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,135 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useCallback, useEffect, useRef, useState} from "react";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export interface ButtonSettings {
+  /* Show back to top button */
+  show?: boolean,
+  /* How far to position from the right side of the screen */
+  right?: string,
+  /* How far to position from the bottom of the screen */
+  bottom?: string,
+  /* The limit to reveal the back to top button */
+  limit?: number,
+  /* If there is a secondary element that needs to be scrolled for the page  */
+  id? : string
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export interface Props {
+  /* Fetch function to fetch more items */
+  fetch : Function | Promise<any>,
+  /* Whether there are more items to fetch */
+  hasMore : boolean,
+  /* Fetch function is currently fetching new items */
+  searchInProgress : boolean,
+  /* Array of items to map over */
+  items : Array<any>,
+  /* Function to map over items as defined in items parameter */
+  renderMap : Function,
+  /* Class names for different parent div for items */
+  classNames : string,
+  /* Object containing various settings for back to top button */
+  buttonSettings : ButtonSettings,
+  /* Function to return HTML for loading */
+  loaderHTML : Function
+}
+
+export const ShoppablePagination : React.FC<Props> = props => {
+  const loader = useRef(null);
+  const [initiateFetch, setInitiateFetch] = useState(false);
+  const [visible, setVisible] = useState(false);
+
+  const handleObserver = useCallback(<T,>(entries : T) => {
+    // @ts-ignore
+    if (entries[0] && entries[0].isIntersecting) {
+      setInitiateFetch(true); // set true to initiate fetch
+    }
+  }, []);
+
+  // track if scrollable div is in view
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver);
+    if (loader.current) {
+      observer.observe(loader.current);
+    }
+  }, [handleObserver]);
+
+  // initiate fetch if all conditions met
+  useEffect(() => {
+    if (initiateFetch && props.hasMore && !props.searchInProgress) {
+      // @ts-ignore
+      props.fetch();
+    }
+    setInitiateFetch(false);
+  }, [initiateFetch, props.hasMore, props.searchInProgress]);
+
+  // check which dom element needs to be scrolled
+  useEffect(() => {
+    if (props.buttonSettings.id) {
+      const el = document.getElementById(props.buttonSettings.id);
+      // @ts-ignore
+      el.addEventListener('scroll', toggleVisible)
+    } else {
+      window.addEventListener('scroll', toggleVisible);
+    }
+  }, [])
+
+  const toggleVisible = () => {
+    // limit at which the back to top will be revealed
+    const limit = props.buttonSettings.limit ? props.buttonSettings.limit : 300;
+
+    // @ts-ignore
+    let scrolled = props.buttonSettings.id ? document.getElementById(props.buttonSettings.id).scrollTop : document.documentElement.scrollTop;
+    if (scrolled > limit) {
+      setVisible(true);
+    } else if (scrolled <= limit) {
+      setVisible(false);
+    }
+  };
+
+  return (
+    <>
+      {props.classNames ?
+        <div className={props.classNames}>
+          {props.items.map((item, index) => {
+            return props.renderMap(item, index);
+          })}
+        </div> : props.items.map((item, index) => {
+          return props.renderMap(item, index);
+        })
+      }
+      {props.searchInProgress ? props.loaderHTML() : <></>}
+      {props.buttonSettings.show &&
+        <button className={'btn btn-primary back-to-top'}
+                onClick={() => {
+                  if (props.buttonSettings.id) {
+                    // @ts-ignore
+                    document.getElementById(props.buttonSettings.id).scroll({
+                      top: 0,
+                      behavior: 'smooth'
+                    });
+                  }
+                  window.scroll({
+                    top: 0,
+                    behavior: 'smooth'
+                  });
+                }}
+                style={{
+                  display: visible ? 'block' : 'none',
+                  backgroundColor: 'rgb(13, 110, 253)',
+                  position: 'fixed',
+                  width: '44px',
+                  padding: '6px',
+                  right: props.buttonSettings && props.buttonSettings.right ? props.buttonSettings.right : '0',
+                  bottom: props.buttonSettings && props.buttonSettings.bottom ? props.buttonSettings.bottom : '25px',
+                  borderWidth: '1px 0',
+                  zIndex: '10'
+                }}
+        >
+          <img src={'https://storage.googleapis.com/shoppable-mp/arrow_white.webp'}
+               style={{height: '30px', padding: '.5rem'}}
+               alt={'Back to top'}/>
+        </button>
+      }
+      <div id={'scrollableDiv'} ref={loader} style={{height: props.hasMore ? '10px' : '0'}}/>
+    </>
+  )
+}
