458cb5d55f21d54972b8b821b80612641691a8dd
dioripazaj <intellective.dev@gmail.com>
2022-10-19 07:02:26 -0400

    NPM Pub commit v0.3.21

--- dist/index.js
+++ dist/index.js
@@ -1,19 +1,44 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
-const ShoppableInfiniteScroll = props => {
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importStar(require("react"));
+var ShoppableInfiniteScroll = function (props) {
     // @ts-ignore
-    const observer = useRef(null);
-    const [initiateFetch, setInitiateFetch] = useState(false);
+    var observer = (0, react_1.useRef)(null);
+    var _a = (0, react_1.useState)(false), initiateFetch = _a[0], setInitiateFetch = _a[1];
     // const [loader, setLoader] = useState();
-    const [visible, setVisible] = useState(false);
-    const { fetch, hasMore, searchInProgress, items, buttonSettings, renderMap, loaderHTML, classNames } = props;
+    var _b = (0, react_1.useState)(false), visible = _b[0], setVisible = _b[1];
+    var fetch = props.fetch, hasMore = props.hasMore, searchInProgress = props.searchInProgress, items = props.items, buttonSettings = props.buttonSettings, renderMap = props.renderMap, loaderHTML = props.loaderHTML, classNames = props.classNames;
     // track if scrollable div is in view
-    const lastItemRef = useCallback((node) => {
+    var lastItemRef = (0, react_1.useCallback)(function (node) {
         var _a, _b;
         if (observer.current)
             (_a = observer.current) === null || _a === void 0 ? void 0 : _a.disconnect();
         // Error: TS2532: Object is possibly 'undefined'.
         // why it should try to use `observer.current.disconnect()` after if(observer.current) condition
-        observer.current = new IntersectionObserver(entries => {
+        observer.current = new IntersectionObserver(function (entries) {
             //Error: TS2322: Type 'IntersectionObserver' is not assignable to type 'undefined'.
             if (entries[0].isIntersecting && hasMore) {
                 setInitiateFetch(true); // set true to initiate fetch
@@ -24,7 +49,7 @@ const ShoppableInfiniteScroll = props => {
         // Error TS2532: Object is possibly 'undefined'.
     }, [initiateFetch, hasMore, searchInProgress]);
     // initiate fetch if all conditions met
-    useEffect(() => {
+    (0, react_1.useEffect)(function () {
         console.log('fetch: ');
         if (initiateFetch && hasMore && !searchInProgress) {
             // @ts-ignore
@@ -33,9 +58,9 @@ const ShoppableInfiniteScroll = props => {
         setInitiateFetch(false);
     }, [initiateFetch, hasMore, searchInProgress]);
     // check which dom element needs to be scrolled
-    useEffect(() => {
+    (0, react_1.useEffect)(function () {
         if (buttonSettings.id) {
-            const el = document.getElementById(buttonSettings.id);
+            var el = document.getElementById(buttonSettings.id);
             // @ts-ignore
             el.addEventListener('scroll', toggleVisible);
         }
@@ -43,11 +68,11 @@ const ShoppableInfiniteScroll = props => {
             window.addEventListener('scroll', toggleVisible);
         }
     }, [buttonSettings.id]);
-    const toggleVisible = () => {
+    var toggleVisible = function () {
         // limit at which the back to top will be revealed
-        const limit = buttonSettings.limit ? buttonSettings.limit : 300;
+        var limit = buttonSettings.limit ? buttonSettings.limit : 300;
         // @ts-ignore
-        let scrolled = buttonSettings.id ? document.getElementById(buttonSettings.id).scrollTop : document.documentElement.scrollTop;
+        var scrolled = buttonSettings.id ? document.getElementById(buttonSettings.id).scrollTop : document.documentElement.scrollTop;
         if (scrolled > limit) {
             setVisible(true);
         }
@@ -56,18 +81,18 @@ const ShoppableInfiniteScroll = props => {
         }
     };
     return (items.length > 0 ?
-        React.createElement(React.Fragment, null,
+        react_1.default.createElement(react_1.default.Fragment, null,
             classNames ?
-                React.createElement("div", { className: classNames }, items.map((item, index) => {
+                react_1.default.createElement("div", { className: classNames }, items.map(function (item, index) {
                     return renderMap(item, index);
                 }))
                 :
-                    items.map((item, index) => {
+                    items.map(function (item, index) {
                         return renderMap(item, index);
                     }),
-            searchInProgress ? loaderHTML() : React.createElement(React.Fragment, null),
+            searchInProgress ? loaderHTML() : react_1.default.createElement(react_1.default.Fragment, null),
             buttonSettings.show &&
-                React.createElement("button", { className: 'btn btn-primary back-to-top', onClick: () => {
+                react_1.default.createElement("button", { className: 'btn btn-primary back-to-top', onClick: function () {
                         if (buttonSettings.id) {
                             // @ts-ignore
                             document.getElementById(buttonSettings.id).scroll({
@@ -90,8 +115,9 @@ const ShoppableInfiniteScroll = props => {
                         borderWidth: '1px 0',
                         zIndex: '10'
                     } },
-                    React.createElement("img", { src: 'https://storage.googleapis.com/shoppable-mp/arrow_white.webp', style: { height: '30px', padding: '.5rem' }, alt: 'Back to top' })),
-            React.createElement("div", { ref: lastItemRef, style: { height: hasMore ? '10px' : '0' } }))
+                    react_1.default.createElement("img", { src: 'https://storage.googleapis.com/shoppable-mp/arrow_white.webp', style: { height: '30px', padding: '.5rem' }, alt: 'Back to top' })),
+            react_1.default.createElement("div", { ref: lastItemRef, style: { height: hasMore ? '10px' : '0' } }))
         : null);
 };
-export default ShoppableInfiniteScroll;
+exports.default = ShoppableInfiniteScroll;
+//# sourceMappingURL=index.js.map
