eb290e58b9585b2f76e6655d1d1f84c98b0576e5
Harun Daloglu <harund@gmail.com>
2025-01-13 17:36:16 -0500

    fixed the backto top button icon

--- src/index.tsx
+++ src/index.tsx
@@ -143,10 +143,9 @@ const ShoppableInfiniteScroll: React.FC<Props> = props => {
                               zIndex: '10'
                           }}
                   >
-                    <img src={'https://storage.googleapis.com/shoppable-mp/arrow_white.webp'}
-                         style={{height: '30px', padding: '.5rem'}}
-                         alt={'Back to top'}
-                    />
+                    <svg style={{padding:"0.5rem"}} height="30px" width="30px" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 185.343 185.343" fill="currentColor" stroke="currentColor" transform="rotate(270)">
+                      <path d="M51.707,185.343c-2.741,0-5.493-1.044-7.593-3.149c-4.194-4.194-4.194-10.981,0-15.175 l74.352-74.347L44.114,18.32c-4.194-4.194-4.194-10.987,0-15.175c4.194-4.194,10.987-4.194,15.18,0l81.934,81.934 c4.194,4.194,4.194,10.987,0,15.175l-81.934,81.939C57.201,184.293,54.454,185.343,51.707,185.343z"></path>
+                    </svg>
                   </button>
                 }
                 <div ref={lastItemRef!} style={{height: hasMore ? '10px' : '0'}}/>
